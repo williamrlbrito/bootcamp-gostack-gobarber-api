694ec55155879d5978700c16f883d4e655ba247a
William Brito <williamrldb@gmail.com>
2020-02-26 22:45:48 -0400

    --Gerando hash de senha

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcript from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,11 +7,18 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN
       },
       { sequelize }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcript.hash(user.password, 8);
+      }
+    });
   }
 }
 
