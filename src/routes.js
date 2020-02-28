1545ed00568a8d00574d948a5dc2b212796f2329
William Brito <williamrldb@gmail.com>
2020-02-27 20:59:24 -0400

    --Middleware e autenticação

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionControler from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionControler.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
