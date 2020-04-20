9b7600c3e90ebf9f7866e1a7834343abf3e968e2
William Brito <williamrldb@gmail.com>
2020-04-20 17:13:09 -0400

    --Monitorando falhas na fila

--- src/app/jobs/CancellationMail.js
+++ src/app/jobs/CancellationMail.js
@@ -11,9 +11,7 @@ class CancellationMail {
   async handle({ data }) {
     const { appointment } = data;
 
-    console.log('A fila executou!');
-
-    await Mail.sendMail({
+    await Mail.senddMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
       template: 'cancellation',
