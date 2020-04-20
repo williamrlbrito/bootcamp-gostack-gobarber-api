0171bb724dfc4626b6182f412481eb2b55b74c50
William Brito <williamrldb@gmail.com>
2020-04-20 15:07:57 -0400

    --Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '43d432da079f0d',
+    pass: '08e33b28aa9a84'
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+};
